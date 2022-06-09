75952fdd86d50bdbd10b451fdb6d9c6903e4dfa2
rishinair19 <rishinair19@gmail.com>
2022-06-09 15:19:10 +0530

    added fixes

--- usercreation.py
+++ usercreation.py
@@ -2,10 +2,8 @@ from airflow.operators.bash import BashOperator
 from datetime import timedelta, datetime
 from airflow import DAG
 
-user_creation = 'airflow users create --username Rishi --firstname Rishi --lastname Nair --role Admin --email rishi.nair@deepintent.com --password rishi'
-
-user_creation_airflow = BashOperator(
-   task_id='user_create', 
-   bash_command=user_creation,
-   dag=dag,
-)
+t2 = BashOperator(
+    task_id='user_create',
+    bash_command='airflow users create --username Rishi --firstname Rishi --lastname Nair --role Admin --email rishi.nair@deepintent.com --password rishi',
+    retries=3,
+    dag=dag)
