c3dc20c06c013d1e800de7466e3f0c39ba6625ef
rishinair19 <rishinair19@gmail.com>
2022-06-09 14:36:03 +0530

    deleting

--- user-creation.py
+++
@@ -1,8 +0,0 @@
-from airflow.operators.bash import BashOperator
-from datetime import timedelta, datetime
-from airflow import DAG
-
-user_create = BashOperator(
-    task_id='user_creation',
-    bash_command='airflow users create --username Rishi --firstname Rishi --lastname Nair --role Admin --email rishi.nair@deepintent.com --password rishi'
-)
